77a420888fcc1159e4ba473d8d4545b5dd76e48d
josephwu1994 <josephwu1994@gmail.com>
2019-05-01 19:57:38 -0700

    implement all features

--- public/detail.js
+++ public/detail.js
@@ -1,16 +1,75 @@
 window.addEventListener('DOMContentLoaded', (e) => {
-  setTimeout(() => {
-    document.getElementById('title').style.opacity = 1;
-    document.getElementById('description').style.opacity = 1;
-    Object.values(document.getElementsByTagName('i')).forEach(el => {
-        el.style.opacity = 1
-    })
-  },500)
-  const tags = project.tags[0]
   fetch(`http://api.hackaday.io/v1/users/${project.owner_id}?api_key=${key}`)
   .then(res => res.json())
   .then(user => {
-    document.getElementsByTagName('img')[0].setAttribute('src', user.image_url)
-    document.getElementById('username').innerHTML = user.username;
+    document.getElementById('profilepic').setAttribute('src', user.image_url)
+    document.getElementById('username').innerHTML = user.username
+  })
+  setTimeout(() => {
+    document.getElementById('title').style.opacity = 1
+    document.getElementById('description').style.opacity = 1
+    Object.values(document.getElementsByTagName('i')).forEach(el => {
+        el.style.opacity = 1
+    })
+    document.getElementById('username').style.opacity = 1
+    document.getElementById('profilepic').style.opacity = 1
+    document.getElementById('recommendation').style.opacity = 1
+  },500)
+  const tag = project.tags[0]
+  fetch(`http://api.hackaday.io/v1/search/users?api_key=${key}&search_term=${tag}&per_page=5`)
+  .then(res => res.json())
+  .then(result => {
+    console.log(result)
+    const users = result.users
+    if(users.length === 0) {
+      const node = document.createElement('h1')
+      node.innerHTML = 'No Recommended Users'
+      document.getElementById('user-suggestion').appendChild(node)
+    } else {
+      users.forEach(user => {
+        const div = document.createElement('div')
+        div.setAttribute('class','suggested-user')
+        const img = document.createElement('img')
+        img.setAttribute('src', user.image_url)
+        const userName = document.createElement('h4')
+        userName.innerHTML = truncateText(user.username)
+        div.appendChild(img)
+        div.appendChild(userName)
+        document.getElementById('user-suggestion').appendChild(div)
+      })
+    }
+  })
+
+  fetch(`http://api.hackaday.io/v1/search/projects?api_key=${key}&search_term=${tag}&per_page=5`)
+  .then(res => res.json())
+  .then(result => {
+    const projects = result.projects
+    if(projects.length === 0) {
+      const node = document.createElement('h1')
+      node.innerHTML = 'No Recommended Projects'
+      document.getElementById('project-suggestion').appendChild(node)
+    } else {
+      projects.forEach(project => {
+        const div = document.createElement('div')
+        div.setAttribute('class','suggested-project')
+        const img = document.createElement('img')
+        img.setAttribute('src', project.image_url)
+        const projectName = document.createElement('h4')
+        projectName.innerHTML = truncateText(project.name)
+        div.appendChild(img)
+        div.appendChild(projectName)
+        document.getElementById('project-suggestion').appendChild(div)
+      })
+    }
   })
 })  
+
+const truncateText = (text) => {
+  const max = 23;
+  if(text === null) return 'no username';
+  if(text.length > max) {
+    return text.substr(0,max)+'...'
+  } else {
+    return text
+  }
+}
--- routes/projects.js
+++ routes/projects.js
@@ -18,36 +18,12 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const id = req.params.id
-  // fetch(`http://api.hackaday.io/v1/projects/${id}?api_key=${process.env.API_KEY}`)
-  // .then(res => res.json()) 
-  // .then(project => {
-  //   res.render('projects/detail', {
-  //     project
-  //   })
-  // })
-  res.render('detail', {
-    project: { id: 5373,
-      url: 'https://hackaday.io/project/5373-hack-chat',
-      owner_id: 182178,
-      name: 'Hack Chat',
-      summary:
-       'Use the team chat to talk about your projects and find collaborators for whatever you are working on.',
-      description:
-       'If you need help on your project you&apos;ve come to the right place. \r\nClick on the &quot;Public Chat&quot; button on the left top and you&apos;re automatically in. \r\n\r\nThose that just want to talk about hacking have found the place as well. All are welcome!',
-      image_url: 'https://cdn.hackaday.io/images/7255311484679597966.png',
-      views: 53591,
-      comments: 193,
-      followers: 2519,
-      skulls: 1359,
-      logs: 0,
-      details: 1,
-      instruction: 1,
-      components: 0,
-      images: 3,
-      created: 1429655720,
-      updated: 1556126879,
-      tags: [ 'ongoing project' ] 
-    }
+  fetch(`http://api.hackaday.io/v1/projects/${id}?api_key=${process.env.API_KEY}`)
+  .then(res => res.json()) 
+  .then(project => {
+    res.render('detail', {
+      project
+    })
   })
 })
 
